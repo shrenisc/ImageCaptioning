41cc082562609250e3bf525ac7444d248dcc171f
shrenisc <shreya.shastry@gmail.com>
2024-04-28 22:58:32 +0530

    EVERYTHING IS WORKING LESSGOOOOOOOOOOO IM SO HAPPPPPYYY

--- app.py
+++ app.py
@@ -1,52 +1,52 @@
-from flask import Flask, render_template, request
-import tensorflow as tf
-import pickle
-from tensorflow.keras.applications.vgg16 import VGG16
-from tensorflow.keras.applications.vgg16 import preprocess_input
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.models import Model
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-import numpy as np
-import os
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
 from PIL import Image
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model,Model
+from tensorflow.keras.preprocessing.text import Tokenizer
+import pickle
 
 app = Flask(__name__)
 
-# Load the modified VGG16 model
-vgg16 = VGG16()
-vgg16 = Model(inputs=vgg16.inputs, outputs=vgg16.layers[-2].output)
+BASE_DIR = './'
+UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+MAX_CONTENT_LENGTH = 1024 * 1024
 
-model = tf.keras.models.load_model('image_captioning_model.h5')  # Replace with your model path
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = MAX_CONTENT_LENGTH
 
-# Load the tokenizer
-tokenizer_path = 'features.pkl'  # Replace with your tokenizer path
-with open(tokenizer_path, 'rb') as f:
+# Load the trained model and tokenizer
+model = load_model('image_captioning_model.h5')
+tokenizer = Tokenizer()
+with open(os.path.join(BASE_DIR, 'features.pkl'), 'rb') as f:
+    features = pickle.load(f)
+with open(os.path.join(BASE_DIR, 'tokenizer.pkl'), 'rb') as f:
     tokenizer = pickle.load(f)
 
-max_length = 33  # Replace with your max_length
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def preprocess_img(img_path):
-    img = Image.open(img_path)
-    img = img.resize((224, 224))
-    img = np.expand_dims(img, axis=0)
-    img = preprocess_input(img)
-    return img
+def index_to_word(integer, tokenizer):
+    for word, index in tokenizer.word_index.items():
+        if index == integer:
+            return word
+    return None
 
-def generate_caption(img_path):
-    processed_img = preprocess_img(img_path)
-    features = vgg16.predict(processed_img)
-    caption = predict_caption(model, features, tokenizer, max_length)
-    return caption
-
-def predict_caption(model, photo, tokenizer, max_length):
+def predict_caption(model, image, tokenizer, max_length):
     in_text = 'startseq'
     for i in range(max_length):
         sequence = tokenizer.texts_to_sequences([in_text])[0]
         sequence = pad_sequences([sequence], maxlen=max_length)
-        yhat = model.predict([photo,sequence], verbose=0)
+        yhat = model.predict([image, sequence], verbose=0)
         yhat = np.argmax(yhat)
-        word = word_for_id(yhat, tokenizer)
+        word = index_to_word(yhat, tokenizer)
         if word is None:
             break
         in_text += ' ' + word
@@ -54,25 +54,39 @@ def predict_caption(model, photo, tokenizer, max_length):
             break
     return in_text
 
-def word_for_id(integer, tokenizer):
-    for word, index in tokenizer.word_index.items():
-        if index == integer:
-            return word
-    return None
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    if request.method == 'POST':
-        uploaded_file = request.files['file']
-        if uploaded_file.filename != '':
-            img_path = os.path.join('uploads', uploaded_file.filename)
-            uploaded_file.save(img_path)
-            caption = generate_caption(img_path)
-            return render_template('result.html', image=img_path, caption=caption)
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+        image = load_img(filepath, target_size=(224, 224))
+# restructure the model
+        vgg_model = VGG16()
+        vgg_model = Model(inputs=vgg_model.inputs, outputs=vgg_model.layers[-2].output)
+        # load image
+        # convert image pixels to numpy array
+        image = img_to_array(image)
+        # reshape data for model
+        image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+        # preprocess image for vgg
+        image = preprocess_input(image)
+        # extract features
+        feature = vgg_model.predict(image, verbose=0)
+        # predict from the trained model
+        caption=predict_caption(model, feature, tokenizer, 33)
+        return render_template('result.html', image=filename, caption=caption)
+    else:
+        return 'Invalid file format'
 
 if __name__ == '__main__':
     app.run(debug=True)
